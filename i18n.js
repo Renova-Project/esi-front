d17adae55ee33cedfc224e08492bb5c162c45890
Abderrezague ismail <ki_abderrezague@esi.dz>
2024-05-15 10:38:34 +0100

    -

--- i18n.js
+++ i18n.js
@@ -9,7 +9,7 @@ export default getRequestConfig(async ({ locale }) => {
   if (!locales.includes(locale)) notFound();
 
   return {
-    messages: (await import(`./messages/${locale}.json`)).default,
+    messages: {...((await import(`./messages/${locale}.json`)).default),
     ...((await import(`./messages/clubs/${locale}.json`)).default)
-  };
+  }};
 });
