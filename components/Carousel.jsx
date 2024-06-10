a76f937518e07cf5bd24799753b46c2356a529f1
Mohamed Emir Bouhamar <me.bouhamar@esi-sba.dz>
2024-06-10 13:17:47 +0100

    internship service

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -3,10 +3,15 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-export default function Carousel({ children, settings, refAssignment }) {
+export default function Carousel({
+  children,
+  settings,
+  refAssignment,
+  responsive,
+}) {
   return (
     <div className="relative">
-      <Slider ref={refAssignment} {...settings}>
+      <Slider ref={refAssignment} {...settings} responsive={responsive}>
         {children}
       </Slider>
     </div>
