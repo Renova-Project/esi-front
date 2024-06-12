937a762a8c786609de8f1524eb30457c7d4e6b21
Abderrezague ismail <ki_abderrezague@esi.dz>
2024-06-12 22:58:21 +0100

    /compus/clubs/details

--- app/[locale]/compus/clubs/StudentsTell.jsx
+++ app/[locale]/compus/clubs/StudentsTell.jsx
@@ -73,11 +73,11 @@ const StudentsTell = () => {
                       }}
                       responsive={[
                         {
-                            breakpoint: 480,
-                            settings: {
-                              slidesToShow: 1,
-                              slidesToScroll: 1,
-                            },
+                          breakpoint: 1024,
+                          settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 1,
+                          },
                         },
                         {
                           breakpoint: 600,
@@ -87,12 +87,12 @@ const StudentsTell = () => {
                           },
                         },
                         {
-                            breakpoint: 1024,
-                            settings: {
-                              slidesToShow: 3,
-                              slidesToScroll: 1,
-                            },
+                          breakpoint: 480,
+                          settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
                           },
+                        },
                       ]}
                     >
                      <Tell nextSlide={next}/>
