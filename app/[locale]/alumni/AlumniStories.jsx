9988f36b83a4eb709d8bbb06d4d168af56a221ec
Abderrezague ismail <ki_abderrezague@esi.dz>
2024-06-13 02:14:59 +0100

    /alumni

--- app/[locale]/alumni/AlumniStories.jsx
+++ app/[locale]/alumni/AlumniStories.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 export const AlumniStories = () => {
   return (
     <div className='bg-darkblue'>
-        <div className='container'>
+        <div className='container py-10'>
+            <h1>{this("alumniStories.title")}</h1>
+            <div className='relative'>
 
+            </div>
         </div>
     </div> 
   )
